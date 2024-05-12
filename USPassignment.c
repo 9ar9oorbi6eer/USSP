71a4966b848f6b4dc71dd9996dbe1a2a24455148
zaid <zaid.khalaf2004au@gmail.com>
2024-05-12 17:20:45 +0800

    code can find the usp extention

--- USPassignment.c
+++ USPassignment.c
@@ -7,8 +7,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define MAX_FILES 100 // max number of .usp files to handle
+
 // Global variables
 int pipe_fd[2];  // File descriptors for the pipe
+char *file_names[MAX_FILES];  // Array to store file names
+int file_count = 0;  // Counter for number of files
 
 int main(int argc, char *argv[]) 
 {
@@ -38,19 +42,35 @@ int main(int argc, char *argv[])
 
     // Here you can add the code to read and filter .usp files
     // Placeholder for reading directory entries
-    while ((entry = readdir(dir)) != NULL) // reads each entry in directory until NULL is returned
+    while ((entry = readdir(dir)) != NULL) 
     {
-        if (strstr(entry->d_name, ".usp") != NULL) // checks if it contains extention .usp
-        {
-            // Process each .usp file
-            printf("Found .usp file: %s\n", entry->d_name);
+            if (strstr(entry->d_name, ".usp") != NULL && file_count < MAX_FILES) // check count limit
+            {
+                // Allocate memory for the filename
+                file_names[file_count] = malloc(strlen(entry->d_name) + 1); 
+                if (file_names[file_count] == NULL) // check memory allocation success
+                {
+                    perror("Failed to allocate memory for filename");
+                    exit(EXIT_FAILURE);
+                }
+                strcpy(file_names[file_count], entry->d_name);
+                printf("Found .usp file: %s\n", file_names[file_count]);
+                file_count++;
+            }
         }
-    }
-
     // Close the directory
     closedir(dir);
 
-    // Cleanup and exit
+    // printing the list, making sure stuff is working:
+    printf("\nList of .usp files:\n");
+    for (int i = 0; i < file_count; i++) 
+    {
+        printf("%s\n", file_names[i]);
+    }
+
+    for (int i = 0; i < file_count; i++) {
+        free(file_names[i]);
+    }
     close(pipe_fd[0]);
     close(pipe_fd[1]);
     return EXIT_SUCCESS;
